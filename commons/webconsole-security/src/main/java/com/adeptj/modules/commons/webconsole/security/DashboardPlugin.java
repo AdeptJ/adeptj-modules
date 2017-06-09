e6364508c245eafe7df10e5430d585071041950c
Rakesh Kumar <kumar_rakesh@live.in>
2017-06-09 09:33:03 +0530

    misc updates.

---
+++ commons/webconsole-security/src/main/java/com/adeptj/modules/commons/webconsole/security/DashboardPlugin.java
@@ -0,0 +1,34 @@
+package com.adeptj.modules.commons.webconsole.security;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.webconsole.SimpleWebConsolePlugin;
+import org.apache.felix.webconsole.WebConsoleConstants;
+
+@Component
+@Service(value = Servlet.class)
+@Properties({ @Property(name = WebConsoleConstants.PLUGIN_LABEL, value = "tools"),
+		@Property(name = WebConsoleConstants.PLUGIN_TITLE, value = "AdeptJ Tools") })
+public class DashboardPlugin extends SimpleWebConsolePlugin {
+
+	private static final long serialVersionUID = 8041033223220201144L;
+
+	public DashboardPlugin() {
+		super("tools", "AdeptJ Tools", "Main", (String[]) null);
+	}
+
+	@Override
+	protected void renderContent(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.sendRedirect("/tools");
+	}
+
+}
---
+++ websecurity/shiro-modular/src/main/java/com/adeptj/modularweb/security/shiro/internal/ShiroConfig.java
@@ -0,0 +1,10 @@
+package com.adeptj.modularweb.security.shiro.internal;
+
+public class ShiroConfig {
+
+	// [main] section
+	
+	private String loginUrl;
+	
+	private String unauthorizedUrl;
+}
