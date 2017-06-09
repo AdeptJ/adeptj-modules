bcb6c1d1451ddaebc3eaaed90453eabe2b82a886
Rakesh Kumar <kumar_rakesh@live.in>
2017-06-09 15:29:36 +0530

    capability - returning List from REST method.

--- commons/datasource/src/main/java/com/adeptj/modules/commons/ds/QCIController.java
+++ commons/datasource/src/main/java/com/adeptj/modules/commons/ds/QCIController.java
@@ -8,6 +8,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/qci")
 @Component(immediate = true, service = QCIController.class, property = "osgi.jaxrs.resource.base=api")
@@ -16,11 +18,13 @@ public class QCIController {
 	@GET
 	@Path("/api/hello/{name}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public User hello(@PathParam("name") @Size(min = 3, max = 5) String name) {
+	public List<User> hello(@PathParam("name") @Size(min = 3, max = 5) String name) {
         User user = new User();
         user.setUsername("rakeshk15");
         user.setFirstName("Rakesh");
         user.setLastName("Kumar");
-        return user;
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        return users;
 	}
 }
