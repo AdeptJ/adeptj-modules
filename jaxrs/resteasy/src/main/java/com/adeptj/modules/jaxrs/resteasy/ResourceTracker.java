b321a12bb240ac6ec5c8c6dc8d563dab1bd34139
Rakesh Kumar <kumar_rakesh@live.in>
2017-06-16 12:55:22 +0530

    put a null check on resource reference, overridden the modifiedService.

--- jaxrs/resteasy/src/main/java/com/adeptj/modules/jaxrs/resteasy/ResourceTracker.java
+++ jaxrs/resteasy/src/main/java/com/adeptj/modules/jaxrs/resteasy/ResourceTracker.java
@@ -29,8 +29,12 @@ import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 /**
  * ResourceTracker is an OSGi ServiceTracker which tracks the services annotated with @Path JAX-RS annotation.
+ *
+ * Note: All the registered JAX-RS resources are Singleton by default.
  * 
  * @author Rakesh.Kumar, AdeptJ.
  */
@@ -51,11 +55,22 @@ public class ResourceTracker extends ServiceTracker<Object, Object> {
 	public Object addingService(ServiceReference<Object> reference) {
 		Object resource = super.addingService(reference);
 		LOGGER.info("Adding JAX-RS Resource: [{}]", resource);
-		this.registry.addSingletonResource(resource);
+        Optional.ofNullable(resource).ifPresent(consumer -> this.registry.addSingletonResource(resource));
 		return resource;
 	}
 
-	@Override
+    /**
+     * Removes the given Resource from RESTEasy Registry and registers again.
+     */
+    @Override
+    public void modifiedService(ServiceReference<Object> reference, Object service) {
+        LOGGER.info("Service is modified, removing JAX-RS Resource: [{}]", service);
+        Optional.ofNullable(service).ifPresent(consumer -> this.registry.removeRegistrations(service.getClass()));
+        LOGGER.info("Adding JAX-RS Resource again: [{}]", service);
+        Optional.ofNullable(service).ifPresent(consumer -> this.registry.addSingletonResource(service));
+    }
+
+    @Override
 	public void removedService(ServiceReference<Object> reference, Object service) {
 		super.removedService(reference, service);
 		LOGGER.info("Removing JAX-RS Resource: [{}]", service);
