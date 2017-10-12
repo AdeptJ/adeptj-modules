2288524e4356ee2f424706de93df982580849f25
Rakesh Kumar <kumar_rakesh@live.in>
2017-10-12 10:01:30 +0530

    better exception handling for key loading mechanism.

--- websecurity/jwt/src/main/java/com/adeptj/modules/security/jwt/internal/JwtSigningKeys.java
+++ websecurity/jwt/src/main/java/com/adeptj/modules/security/jwt/internal/JwtSigningKeys.java
@@ -29,7 +29,6 @@ import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
@@ -39,6 +38,7 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
 import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.SystemUtils.USER_DIR;
 
 /**
  * Utility for loading JWT signing key.
@@ -57,8 +57,6 @@ final class JwtSigningKeys {
 
     private static final String ALGO_RSA = "RSA";
 
-    private static final String SYS_PROP_CURRENT_DIR = "user.dir";
-
     private static final String DEFAULT_KEY_FILE = "/default.pem";
 
     static byte[] getHmacSecretKey(SignatureAlgorithm signatureAlgo, String secretKey) throws UnsupportedEncodingException {
@@ -76,51 +74,44 @@ final class JwtSigningKeys {
     }
 
     static PrivateKey getRsaPrivateKey(JwtConfig jwtConfig) throws Exception {
-        KeyFactory keyFactory = KeyFactory.getInstance(ALGO_RSA);
         // 1. try the jwtConfig provided keyFileLocation
-        String keyFileLocation = System.getProperty(SYS_PROP_CURRENT_DIR) +
-                File.separator +
-                jwtConfig.keyFileLocation();
+        String keyFileLocation = USER_DIR + File.separator + jwtConfig.keyFileLocation();
         Logger logger = Loggers.get(JwtSigningKeys.class);
-        logger.info("Loading Key file from location: [{}]", keyFileLocation);
-        PrivateKey privateKey = loadKeyFromLocation(keyFactory, keyFileLocation);
+        KeyFactory keyFactory = KeyFactory.getInstance(ALGO_RSA);
+        PrivateKey privateKey = loadKeyFromLocation(keyFactory, keyFileLocation, logger);
         if (privateKey == null && jwtConfig.useDefaultKey()) {
-            logger.warn("Couldn't load Key file from location [{}], using the default one!!", keyFileLocation);
+            logger.warn("Loading the default Key, please check if that is intended!!");
             // 2. Use the default one that is embedded with this module.
             privateKey = loadDefaultKey(keyFactory, logger);
         }
         if (privateKey == null) {
-            throw new IllegalStateException("Couldn't initialize the SigningKey!!");
+            throw new IllegalStateException("Couldn't initialize the RsaPrivateKey!!");
         }
         return privateKey;
     }
 
-    private static PrivateKey loadKeyFromLocation(KeyFactory keyFactory, String keyFileLocation) throws Exception {
+    private static PrivateKey loadKeyFromLocation(KeyFactory keyFactory, String keyFileLocation, Logger logger) {
         PrivateKey privateKey = null;
-        try (FileInputStream inputStream = new FileInputStream(keyFileLocation)) {
-            privateKey = generatePrivateKey(keyFactory, inputStream);
-        } catch (FileNotFoundException ex) {
-            // Gulp it. Try to load the embedded/default one next.
+        try (InputStream data = new FileInputStream(keyFileLocation)) {
+            privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(encodedKey(data)));
+        } catch (Exception ex) {
+            logger.error("Exception while loading Key file!!", ex);
         }
         return privateKey;
     }
 
     private static PrivateKey loadDefaultKey(KeyFactory keyFactory, Logger logger) {
         PrivateKey privateKey = null;
-        try (InputStream inputStream = JwtServiceImpl.class.getResourceAsStream(DEFAULT_KEY_FILE)) {
-            privateKey = generatePrivateKey(keyFactory, inputStream);
+        try (InputStream data = JwtSigningKeys.class.getResourceAsStream(DEFAULT_KEY_FILE)) {
+            privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(encodedKey(data)));
         } catch (Exception ex) {
             logger.error(ex.getMessage(), ex);
         }
         return privateKey;
     }
 
-    private static PrivateKey generatePrivateKey(KeyFactory keyFactory, InputStream stream) throws Exception {
-        return keyFactory.generatePrivate(new PKCS8EncodedKeySpec(encodedKey(stream)));
-    }
-
-    private static byte[] encodedKey(InputStream stream) throws IOException {
-        return Base64.getDecoder().decode(IOUtils.toString(stream, UTF8)
+    private static byte[] encodedKey(InputStream data) throws IOException {
+        return Base64.getDecoder().decode(IOUtils.toString(data, UTF8)
                 .replace(KEY_HEADER, EMPTY)
                 .replace(KEY_FOOTER, EMPTY)
                 .replaceAll(REGEX_SPACE, EMPTY)
