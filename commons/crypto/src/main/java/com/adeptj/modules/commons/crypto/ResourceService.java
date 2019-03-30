dad6babea129ce7614982ccd2d11a4271bbf93a5
Rakesh Kumar <kumar_rakesh@live.in>
2019-03-30 07:26:34 +0530

    Massive Cleanup
    Cleanup the template-engines, mvc. REST only server as of now.

--- commons/crypto/src/main/java/com/adeptj/modules/commons/crypto/ResourceService.java
+++
@@ -1,9 +0,0 @@
-package com.adeptj.modules.commons.crypto;
-
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardResource;
-
-@HttpWhiteboardResource(pattern = {"/files/*"}, prefix = "WEB-INF")
-@Component(service = ResourceService.class)
-public class ResourceService {
-}
--- mvc/src/main/java/com/adeptj/modules/mvc/TemplateEngineConstants.java
+++
@@ -1,8 +0,0 @@
-package com.adeptj.modules.mvc;
-
-final class TemplateEngineConstants {
-
-    static final String TEMPLATE_HEADER = "Mustache-Templates-Location";
-
-    static final String I18N_HEADER = "Mustache-I18n-Location";
-}
