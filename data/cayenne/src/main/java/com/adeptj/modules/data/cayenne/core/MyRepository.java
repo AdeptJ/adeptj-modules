47743300c52db4297e634afa34dcbcc425423fc4
Rakesh Kumar <kumar_rakesh@live.in>
2021-03-14 22:31:11 +0530

    Update MyRepository.java

--- data/cayenne/src/main/java/com/adeptj/modules/data/cayenne/core/MyRepository.java
+++ data/cayenne/src/main/java/com/adeptj/modules/data/cayenne/core/MyRepository.java
@@ -3,8 +3,11 @@ package com.adeptj.modules.data.cayenne.core;
 import com.adeptj.modules.data.cayenne.CayenneRepository;
 import com.adeptj.modules.data.cayenne.model.Users;
 import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.query.ObjectSelect;
 import org.apache.cayenne.query.SelectById;
 import org.osgi.service.component.annotations.Component;
+
+import java.util.List;
 
 @Component
 public class MyRepository extends AbstractCayenneRepository<Users> implements CayenneRepository<Users> {
@@ -12,6 +15,12 @@ public class MyRepository extends AbstractCayenneRepository<Users> implements Ca
     public Users getUserById() {
         return this.doInCayenne(cayenne -> {
             return SelectById.query(Users.class, 7).selectOne(cayenne.newContext());
+        });
+    }
+
+    public List<Users> getAllUsers() {
+        return this.doInCayenne(cayenne -> {
+            return ObjectSelect.query(Users.class).select(cayenne.newContext());
         });
     }
 
