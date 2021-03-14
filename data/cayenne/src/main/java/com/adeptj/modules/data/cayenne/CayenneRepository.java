25fad41834cccc1fd4fe8880c30dbaadce9bbda0
Rakesh Kumar <kumar_rakesh@live.in>
2021-03-14 22:14:43 +0530

    added some methods in AbstractCayenneRepository.

--- data/cayenne/src/main/java/com/adeptj/modules/data/cayenne/CayenneRepository.java
+++ data/cayenne/src/main/java/com/adeptj/modules/data/cayenne/CayenneRepository.java
@@ -1,7 +1,21 @@
 package com.adeptj.modules.data.cayenne;
 
+import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.jetbrains.annotations.NotNull;
 import org.osgi.annotation.versioning.ConsumerType;
 
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 @ConsumerType
-public interface CayenneRepository {
+public interface CayenneRepository<T> {
+
+    <E> E doInCayenne(@NotNull Function<ServerRuntime, E> function);
+
+    void doInCayenne(@NotNull Consumer<ServerRuntime> consumer);
+
+    T findById(Object id, Class<T> entity);
+
+    List<T> findMany(Class<T> entity);
 }
--- data/cayenne/src/main/java/com/adeptj/modules/data/cayenne/core/AbstractCayenneRepository.java
+++ data/cayenne/src/main/java/com/adeptj/modules/data/cayenne/core/AbstractCayenneRepository.java
@@ -2,18 +2,47 @@ package com.adeptj.modules.data.cayenne.core;
 
 import com.adeptj.modules.data.cayenne.CayenneRepository;
 import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.apache.cayenne.query.ObjectSelect;
+import org.apache.cayenne.query.SelectById;
+import org.apache.commons.lang3.Validate;
+import org.jetbrains.annotations.NotNull;
 import org.osgi.annotation.versioning.ConsumerType;
 
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 @ConsumerType
-public class AbstractCayenneRepository implements CayenneRepository {
+public class AbstractCayenneRepository<T> implements CayenneRepository<T> {
 
-    private ServerRuntime cayenne;
+    private volatile ServerRuntime cayenne;
 
-    public ServerRuntime getCayenne() {
+    protected ServerRuntime getCayenne() {
+        Validate.isTrue((this.cayenne != null), "Cayenne ServerRuntime is null!!");
         return cayenne;
     }
 
     public void setCayenne(ServerRuntime cayenne) {
         this.cayenne = cayenne;
+    }
+
+    @Override
+    public <E> E doInCayenne(@NotNull Function<ServerRuntime, E> function) {
+        return function.apply(this.getCayenne());
+    }
+
+    @Override
+    public void doInCayenne(@NotNull Consumer<ServerRuntime> consumer) {
+        consumer.accept(this.getCayenne());
+    }
+
+    @Override
+    public T findById(Object id, Class<T> entity) {
+        return SelectById.query(entity, id).selectOne(this.cayenne.newContext());
+    }
+
+    @Override
+    public List<T> findMany(Class<T> entity) {
+        return ObjectSelect.query(entity).select(this.cayenne.newContext());
     }
 }
