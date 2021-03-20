26ef804bcbdfde0b95153ff42531308bd3be56c1
Rakesh Kumar <kumar_rakesh@live.in>
2021-03-20 15:18:45 +0530

    updated jdbc ds module.

--- commons/jdbc/src/main/java/com/adeptj/modules/commons/jdbc/service/internal/HikariDataSourceService.java
+++ commons/jdbc/src/main/java/com/adeptj/modules/commons/jdbc/service/internal/HikariDataSourceService.java
@@ -21,7 +21,9 @@
 package com.adeptj.modules.commons.jdbc.service.internal;
 
 import com.adeptj.modules.commons.jdbc.service.DataSourceService;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.jetbrains.annotations.NotNull;
@@ -38,9 +40,9 @@ import java.lang.invoke.MethodHandles;
 import static org.osgi.service.component.annotations.ConfigurationPolicy.REQUIRE;
 
 /**
- * Provides {@link com.zaxxer.hikari.HikariDataSource} as the JDBC {@link DataSource} implementation.
+ * Provides {@link HikariDataSource} as the JDBC {@link DataSource} implementation.
  * <p>
- * The {@link com.zaxxer.hikari.HikariDataSource} is configured by the {@link HikariDataSourceService}.
+ * The {@link HikariDataSource} is configured by the {@link HikariDataSourceService}.
  * <p>
  *
  * @author Rakesh.Kumar, AdeptJ
@@ -50,6 +52,8 @@ import static org.osgi.service.component.annotations.ConfigurationPolicy.REQUIRE
 public class HikariDataSourceService implements DataSourceService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private static final String EQ = "=";
 
     private final HikariDataSource dataSource;
 
@@ -62,8 +66,7 @@ public class HikariDataSourceService implements DataSourceService {
     @Activate
     public HikariDataSourceService(DataSourceConfig config) {
         try {
-            Validate.isTrue(StringUtils.isNotEmpty(config.poolName()), "JDBC Pool Name can't be null!!");
-            this.dataSource = DataSources.createHikariDataSource(config);
+            this.dataSource = this.createHikariDataSource(config);
             LOGGER.info("HikariDataSource: [{}] initialized!!", config.poolName());
         } catch (Exception ex) { // NOSONAR
             LOGGER.error(ex.getMessage(), ex);
@@ -77,6 +80,30 @@ public class HikariDataSourceService implements DataSourceService {
     @Override
     public @NotNull DataSource getDataSource() {
         return new DataSourceWrapper(this.dataSource);
+    }
+
+    @NotNull
+    private HikariDataSource createHikariDataSource(DataSourceConfig config) {
+        Validate.isTrue(StringUtils.isNotEmpty(config.poolName()), "JDBC Pool Name can't be null!!");
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setPoolName(config.poolName());
+        hikariConfig.setJdbcUrl(config.jdbcUrl());
+        hikariConfig.setDriverClassName(config.driverClassName());
+        hikariConfig.setUsername(config.username());
+        hikariConfig.setPassword(config.password());
+        hikariConfig.setAutoCommit(config.autoCommit());
+        hikariConfig.setConnectionTimeout(config.connectionTimeout());
+        hikariConfig.setIdleTimeout(config.idleTimeout());
+        hikariConfig.setMaxLifetime(config.maxLifetime());
+        hikariConfig.setMinimumIdle(config.minimumIdle());
+        hikariConfig.setMaximumPoolSize(config.maximumPoolSize());
+        for (String row : config.dataSourceProperties()) {
+            String[] mapping = StringUtils.split(row, EQ);
+            if (ArrayUtils.getLength(mapping) == 2) {
+                hikariConfig.addDataSourceProperty(mapping[0].trim(), mapping[1].trim());
+            }
+        }
+        return new HikariDataSource(hikariConfig);
     }
 
     // <<------------------------------------- OSGi Internal  -------------------------------------->>
