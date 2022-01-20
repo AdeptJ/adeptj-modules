b47eaaddd070fa2526de7c078c2c4f3c1dc8e18e
Rakesh Kumar <kumar_rakesh@live.in>
2022-01-20 08:43:36 +0530

    Added licence header.

--- commons/logging/src/main/java/com/adeptj/modules/commons/logging/internal/LoggerConfig.java
+++ commons/logging/src/main/java/com/adeptj/modules/commons/logging/internal/LoggerConfig.java
@@ -1,3 +1,22 @@
+/*
+###############################################################################
+#                                                                             #
+#    Copyright 2016, AdeptJ (http://www.adeptj.com)                           #
+#                                                                             #
+#    Licensed under the Apache License, Version 2.0 (the "License");          #
+#    you may not use this file except in compliance with the License.         #
+#    You may obtain a copy of the License at                                  #
+#                                                                             #
+#        http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                             #
+#    Unless required by applicable law or agreed to in writing, software      #
+#    distributed under the License is distributed on an "AS IS" BASIS,        #
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+#    See the License for the specific language governing permissions and      #
+#    limitations under the License.                                           #
+#                                                                             #
+###############################################################################
+*/
 package com.adeptj.modules.commons.logging.internal;
 
 import org.osgi.service.metatype.annotations.AttributeDefinition;
@@ -40,7 +59,7 @@ public @interface LoggerConfig {
     )
     boolean logger_additivity();
 
-    // name hint non editable property
+    // name hint non-editable property
     String webconsole_configurationFactory_nameHint() default
             "Logger ({" + "logger.names" + "}" + ": " + "{" + "logger.level" + "})"; // NOSONAR
 }
