37d70224074e85bbded3ea669d1f0005fee915e8
Rakesh Kumar <kumar_rakesh@live.in>
2022-02-16 08:52:25 +0530

    added serialVersionUID

--- commons/email/src/main/java/com/adeptj/modules/commons/email/EmailException.java
+++ commons/email/src/main/java/com/adeptj/modules/commons/email/EmailException.java
@@ -2,7 +2,9 @@ package com.adeptj.modules.commons.email;
 
 public class EmailException extends RuntimeException {
 
-    public EmailException(Throwable cause) {
+	private static final long serialVersionUID = -3117803548267393781L;
+
+	public EmailException(Throwable cause) {
         super(cause);
     }
 }
