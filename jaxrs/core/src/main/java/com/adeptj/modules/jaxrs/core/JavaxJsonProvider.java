7906b1f7a50ff0e185e5d5040893e9d7d35fa3bf
Rakesh Kumar <kumar_rakesh@live.in>
2022-08-21 08:52:52 +0530

    Minor updates.

--- jaxrs/core/src/main/java/com/adeptj/modules/jaxrs/core/JavaxJsonProvider.java
+++ jaxrs/core/src/main/java/com/adeptj/modules/jaxrs/core/JavaxJsonProvider.java
@@ -7,13 +7,13 @@ import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
 import javax.json.spi.JsonProvider;
-import javax.json.stream.JsonGenerator;
 import javax.json.stream.JsonGeneratorFactory;
 import javax.json.stream.JsonParserFactory;
 import java.util.Map;
 
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
+import static javax.json.stream.JsonGenerator.PRETTY_PRINTING;
 
 /**
  * Provides Jakarta(javax namespace) {@link Jsonb} and other objects from Jakarta Json-P.
@@ -41,16 +41,16 @@ public class JavaxJsonProvider {
 
     static {
         JsonProvider provider = JsonProvider.provider();
-        JSONB = JsonbBuilder.newBuilder()
-                .withProvider(provider)
-                .withConfig(new JsonbConfig().withFormatting(FALSE).withEncoding(UTF8))
-                .build();
-        Map<String, ?> config = Map.of(JsonGenerator.PRETTY_PRINTING, TRUE);
+        Map<String, ?> config = Map.of(PRETTY_PRINTING, TRUE);
         READER_FACTORY = provider.createReaderFactory(null);
         WRITER_FACTORY = provider.createWriterFactory(config);
         GENERATOR_FACTORY = provider.createGeneratorFactory(config);
         PARSER_FACTORY = provider.createParserFactory(null);
         JSON_BUILDER_FACTORY = provider.createBuilderFactory(null);
+        JSONB = JsonbBuilder.newBuilder()
+                .withProvider(provider)
+                .withConfig(new JsonbConfig().withFormatting(FALSE).withEncoding(UTF8))
+                .build();
     }
 
     public static Jsonb getJsonb() {
