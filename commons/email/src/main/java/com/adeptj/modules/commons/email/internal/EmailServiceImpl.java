8fe785f48d62ebca033b5f2f7e2ba59cf9e61abb
Rakesh Kumar <kumar_rakesh@live.in>
2022-12-24 16:36:19 +0530

    Mail Session is initialized lazily as the eager init was causing class loading issues.

--- commons/email/src/main/java/com/adeptj/modules/commons/email/internal/EmailServiceImpl.java
+++ commons/email/src/main/java/com/adeptj/modules/commons/email/internal/EmailServiceImpl.java
@@ -22,36 +22,42 @@ public class EmailServiceImpl extends Authenticator implements EmailService {
 
     private final EmailConfig config;
 
-    private final Session session;
-
     private final ExecutorService emailExecutorService;
+
+    private Session session;
 
     @Activate
     public EmailServiceImpl(@NotNull EmailConfig config) {
         this.config = config;
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", config.smtp_host());
-        props.put("mail.smtp.port", config.smtp_port());
-        props.put("mail.debug", Boolean.toString(config.debug()));
-        this.session = Session.getInstance(props, this);
         this.emailExecutorService = Executors.newFixedThreadPool(config.thread_pool_size());
     }
 
     @Override
     public void sendEmail(@NotNull EmailInfo emailInfo) {
-        new EmailSender(emailInfo, this.config.default_from_address(), this.session).send();
+        new EmailSender(emailInfo, this.config.default_from_address(), this.getSession()).send();
     }
 
     @Override
     public void sendEmailAsync(@NotNull EmailInfo emailInfo) {
-        this.emailExecutorService.execute(new EmailSender(emailInfo, this.config.default_from_address(), this.session));
+        this.emailExecutorService.execute(new EmailSender(emailInfo, this.config.default_from_address(), this.getSession()));
     }
 
     @Override
     protected PasswordAuthentication getPasswordAuthentication() {
         return new PasswordAuthentication(this.config.username(), this.config.password());
+    }
+
+    private Session getSession() {
+        if (this.session == null) {
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", this.config.smtp_host());
+            props.put("mail.smtp.port", this.config.smtp_port());
+            props.put("mail.debug", Boolean.toString(this.config.debug()));
+            this.session = Session.getInstance(props, this);
+        }
+        return this.session;
     }
 
     // <<------------------------------------------ OSGi Internal  ------------------------------------------->>
